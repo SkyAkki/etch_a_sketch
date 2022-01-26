55fe5804e591f3e1c9cdc7f46845f9d4c48bca86
Akash Saxena <akashsaxena5555@gmail.com>
2022-01-26 16:16:02 +0530

    add hover logic

--- script/script.js
+++ script/script.js
@@ -5,3 +5,10 @@ for (i=0; i<256; i++)
     const newDiv = document.createElement("div");
     newGrid.appendChild(newDiv);
 }
+let hoverDiv = document.querySelectorAll(".container > div")
+hoverDiv.forEach(element => {
+    element.addEventListener("mouseenter", function(event)
+{
+    event.target.style.backgroundColor = "purple"
+});
+});
