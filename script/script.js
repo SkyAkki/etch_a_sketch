c0ad2d6849ba0b548e01dbbe3bd6836842b7a868
Akash Saxena <akashsaxena5555@gmail.com>
2022-01-30 19:38:54 +0530

    add UI javascipt

--- script/script.js
+++ script/script.js
@@ -1,28 +1,79 @@
+let gridSize;
 let newGrid = document.querySelector(".container");
-let i = 0;
-for (i=0; i<256; i++)
-{
-    const newDiv = document.createElement("div");
-    newGrid.appendChild(newDiv);
-}
-let hoverDiv = document.querySelectorAll(".container > div")
+loadGrid(gridSize = 50);
+let hoverDiv = document.querySelectorAll(".container > div");
 let isDown = false;
-hoverDiv.forEach(element => {
-    element.addEventListener("mouseenter", function(event)
-    {
-        element.addEventListener("mousedown", function(event)
+
+function draw(colorChoice)
+{
+    hoverDiv.forEach(element => {
+        element.addEventListener("mouseenter", function(event)
         {
-            isDown = true;
-            event.target.style.backgroundColor = "purple";
-            event.preventDefault();
-        })
-        document.addEventListener("mouseup",function(event)
-        {
-            isDown = false;
-        })
-        if (isDown)
-        {
-            event.target.style.backgroundColor = "purple";
-        }
+            element.addEventListener("mousedown", function(event)
+            {
+                isDown = true;
+                event.target.style.backgroundColor = colorChoice;
+                event.preventDefault();
+            })
+            document.addEventListener("mouseup",function(event)
+            {
+                isDown = false;
+            })
+            if (isDown)
+            {
+                event.target.style.backgroundColor = colorChoice;
+            }
+        });
     });
-});
+}
+function randomColor()
+{
+    
+}
+function pencil()
+{
+    let colorChoice = "purple";
+    draw(colorChoice);
+}
+function eraser()
+{
+    let colorChoice = "white";
+    draw(colorChoice);
+}
+function rainbow()
+{
+    let colorChoice = randomColor();
+    draw(colorChoice);
+}
+function clearall()
+{
+    hoverDiv.forEach(element => {
+        element.style.backgroundColor = "white"
+    })
+}
+function loadGrid(gridSize)
+{
+
+    let i = 0;
+    newheight = 360/gridSize;
+    newpadding = newheight/2;
+    for (i=0; i<gridSize**2; i++)
+    {
+        const newDiv = document.createElement("div");
+        newDiv.style.padding = `${newpadding}px`;
+        newGrid.appendChild(newDiv);
+    }
+}
+function changeGrid()
+{
+    let input = prompt("Please enter a number less than 100 for number of grid cells", "25");
+    inputValue = parseInt(input);
+    if (input != null && inputValue<=100) 
+    {
+    //   while (newGrid.firstChild) 
+    //   {
+    //     newGrid.firstChild.remove()
+    //   }
+      loadGrid(inputValue);
+    }
+}
